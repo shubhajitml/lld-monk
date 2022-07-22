c094f62e72ed2351b2dabcba57e94b99efc76b98
Shubhajit Das <shubhajit.das@dell.com>
2022-07-22 12:52:49 +0530

    initial template

---
+++ src/main/java/com/lld/cardgame/Game.java
@@ -0,0 +1,18 @@
+package com.lld.cardgame;
+
+public class Game {
+    public Game(){
+    }
+
+    public void addCard(String suit, String value){
+    }
+
+    public String cardString(int card){
+        return "";
+    }
+
+    public boolean cardBeats(int cardA, int cardB){
+        return false;
+    }
+
+}
