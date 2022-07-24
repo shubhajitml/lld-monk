6f56bb9a452840e2996ca8b160e4f477843d2e16
Shubhajit Das <shubhajit.das@dell.com>
2022-07-24 14:26:27 +0530

    added Part Three solution - Hand

--- lld-card-game/src/main/java/com/lld/cardgame/Game.java
+++ lld-card-game/src/main/java/com/lld/cardgame/Game.java
@@ -1,16 +1,21 @@
 package com.lld.cardgame;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.lld.cardgame.entities.Card;
+import com.lld.cardgame.entities.Hand;
 import com.lld.cardgame.services.AddJokerService;
+import com.lld.cardgame.services.HandService;
 import com.lld.cardgame.services.PlayingCardService;
 
 public class Game {
     ArrayList<Card> cards;
+    ArrayList<Hand> hands;
 
     public Game(){
         cards = new ArrayList<>();
+        hands = new ArrayList<>();
     }
 
     public void addCard(String suit, String value){
@@ -27,5 +32,21 @@ public class Game {
 
     public void addJoker(String color){
         cards.add(new AddJokerService(color));
+    }
+
+    public void addHand(List<Integer> cardIndices){
+        ArrayList<Card> cardObjects = new ArrayList<>();
+        for(int i : cardIndices){
+            cardObjects.add(cards.get(i));
+        }
+        hands.add(new HandService(cardObjects));
+    }
+
+    public String handString(int handIndice){
+        return hands.get(handIndice).toString();
+    }
+
+    public boolean handBeats(int handA, int handB){
+        return hands.get(handA).compareTo(hands.get(handB)) > 0;
     }
 }
